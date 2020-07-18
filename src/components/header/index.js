9d57fd128b3cd241ee691b583fa51ce65064f9e1
Miroslav <fshn1994@abv.bg>
2020-07-18 23:32:26 +0300

    feat: add routing

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,19 +2,27 @@ import React from 'react'
 import styles from './index.module.css'
 import Link from '../link'
 import logo from '../../images/198fc8a2981818511dd4c313c2176ff8.jpg'
+import getNavigation from '../../utils/navigation'
 
 const Header = () => {
+    const links = getNavigation()
+
     return (
         <header className={styles.nav}>
             <div className={styles.div}>
                 <img className={styles.logo} src={logo}></img>
-                <Link href="#" title="Products" type="header" />
-                <Link href="#" title="New products" type="header" />
-                <Link href="#" title="Brochure" type="header" />
-                <Link href="#" title="Shops" type="header" />
-                <Link href="#" title="About us" type="header" />
-                <Link href="#" title="Contacts" type="header" />
-                <Link href="#" title="Logout" type="header" />
+                {
+                    links.map(navElement => {
+                        return (
+                            <Link
+                                key={navElement.title}
+                                href={navElement.link}
+                                title={navElement.title}
+                                type="header"
+                            />
+                        )
+                    })
+                }
                 <input className={styles.search} type="text" placeholder="Search" />
             </div>
         </header>
