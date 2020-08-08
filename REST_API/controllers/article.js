b373a08fe2347608e618bbf94583a91ea60dc0d2
Miroslav <fshn1994@abv.bg>
2020-08-08 23:11:17 +0300

    feat: change main page design

--- REST_API/controllers/article.js
+++ REST_API/controllers/article.js
@@ -3,7 +3,7 @@ const models = require('../models');
 module.exports = {
     get: {
         getAllArticles: (req, res, next) => {
-            models.article.find().sort('-created_at').limit(4).populate('creatorId')
+            models.article.find().sort('-created_at').limit(8).populate('creatorId')
                 .then((articles) => res.send(articles))
                 .catch(next);
         },
